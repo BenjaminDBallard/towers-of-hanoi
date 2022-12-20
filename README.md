eeb2297ae69a21a391fc9dd4b452edb4d30c1fc9
BenjaminDBallard <benb971@gmail.com>
2022-12-19 23:41:10 -0600

    Connected GUI, minor styling

--- README.md
+++ README.md
@@ -1,72 +1,68 @@
 ## CODE PLAN
 
-\\order of operations:\\
+order of operations:
 
 1. `user input` -> **towersOfHanoi()**
 
-  <!-- -towersOfHanoi(start, end){
-    if (input === legal && move is legal){
-      move the block;
-      check for the win;
-    }
-  } -->
+   -towersOfHanoi(start, end){
+   if (input === legal && move is legal){
+   move the block;
+   check for the win;
+   }
+   }
 
 2. `validate input` -> **validInput()** = check for "a, b, c" only
 
-  <!-- -validInput(input){
-    if (input === a or b or c){
-      tell towersOfHanoi()tm. true
-    }
-  } -->
+   -validInput(input){
+   if (input === a or b or c){
+   tell towersOfHanoi()tm. true
+   }
+   }
 
 3. `validate legal move` -> **isLegal()** = 'if' greater/less than
 
-  <!-- -isLegal(start, end){
-    if ('starts' top item < than 'ends' top item || if 'end' is empty){
-      return true
-    }
-  } -->
+   -isLegal(start, end){
+   if ('starts' top item < than 'ends' top item || if 'end' is empty){
+   return true
+   }
+   }
 
 4. `move block` -> **movePiece()** = pop and push with 'held' variable
 
-  <!-- -movePiece(start, end){
-    held = start.pop()
-    end.push(held)
-  } -->
+   -movePiece(start, end){
+   held = start.pop()
+   end.push(held)
+   }
 
 5. `check for win` -> **checkForWin()** = equality to win state
 
-  <!-- -checkForWin(){
-    if (the array === win state){
-      return true
-    }
-  } -->
+   -checkForWin(){
+   if (the array === win state){
+   return true
+   }
+   }
 
-- repeat or notify win
+6. repeat or notify win
 
-## test case ideas **that are not already in use**
+## Additional test cases
 
 1. move several blocks ✔
 2. test valid inputs ✔
 3. verify restart on win✔
 
-\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
 <!-- ## Checklist -->
 
-✔ <!-- 1. 20pts - **Code Plan** - Include this in a `README.md` file in your folder with comments in your code -->
-✔ <!-- 1. 10pts - **Move Blocks** - User can move "blocks" from column to column -->
-✔ <!-- 1. 20pts - **Illegal Moves** - Prevents larger blocks from stacking on smaller blocks -->
-✔ <!-- 1. 20pts - **Notifies winner** - When all the blocks are stacked into column 2 or 1 the user is alerted they won! -->
-✔<!-- 1. 20pts - **Minimum 3 Unit Tests** - Should be attached to your file the same way Tic, Tac, Toe, PigLatin or Rock Paper Scissors is done. -->
+<!-- 1. 20pts - **Code Plan** - Include this in a `README.md` file in your folder with comments in your code -->
+<!-- 1. 10pts - **Move Blocks** - User can move "blocks" from column to column -->
+<!-- 1. 20pts - **Illegal Moves** - Prevents larger blocks from stacking on smaller blocks -->
+<!-- 1. 20pts - **Notifies winner** - When all the blocks are stacked into column 2 or 1 the user is alerted they won! -->
+<!-- 1. 20pts - **Minimum 3 Unit Tests** - Should be attached to your file the same way Tic, Tac, Toe, PigLatin or Rock Paper Scissors is done. -->
 
-1. 10pts - **Graphical User Interface** - Take this game out of the terminal by adding a User Interface that uses `towersOfHanoi()` function in `index.js`.
+<!-- 1. 10pts - **Graphical User Interface** - Take this game out of the terminal by adding a User Interface that uses `towersOfHanoi()` function in `index.js`. -->
 
 - **Extended Practice Bonus!!**
   - Keeps count of moves as player plays games
   - Sound Effects
   - Peaceful Background
-  - Vertical columns
-  - Proportional Stones/tokens
-
-1. Write 3 tests for the app.
+    ✔ - Vertical columns
+    ✔ - Proportional Stones/tokens
--- index.js
+++ index.js
@@ -1,30 +1,159 @@
 let stone = null;
+let size = null;
+let rowStart = null;
+let rowEnd = null;
 
 // this function is called when a row is clicked.
 const selectRow = (row) => {
   const currentRow = row.getAttribute("data-row");
-
+  // const current = row.lastElementChild.getAttribute("data-size");
   console.log("Yay, we clicked an item", row);
-  console.log("Here is the stone's id: ", row.id);
-  console.log("Here is the stone's data-size: ", currentRow);
+
+  console.log("row id: ", row.id);
+  // console.log("Here is the stone's data-size: ", current);
 
   if (stone === null) {
     pickUpStone(row.id);
   } else {
     dropStone(row.id, stone);
     stone = null;
+    size = null;
+    rowStart = null;
   }
 };
 
 // this function can be called to get the last stone in the stack
 const pickUpStone = (rowID) => {
   const selectedRow = document.getElementById(rowID);
+  rowStart = rowID;
   console.log("selected: ", selectedRow);
   stone = selectedRow.removeChild(selectedRow.lastElementChild);
-  console.log("you picked up: ", stone);
+  size = stone.getAttribute("data-size");
+  console.log("you picked up: ", size);
 };
 
 // this function can be called to drop the held stone
 const dropStone = (rowID, stone) => {
-  document.getElementById(rowID).appendChild(stone);
+  if (isLegal(rowStart, rowID) === true) {
+    rowEnd = rowID;
+    towersOfHanoi(rowStart, rowID);
+    document.getElementById(rowID).appendChild(stone);
+  } else {
+    alert("invalid placement");
+    document.getElementById(rowStart).appendChild(stone);
+  }
+};
+
+// -------------------------------------------------------------
+
+let stacks = {
+  a: [4, 3, 2, 1],
+  b: [],
+  c: [],
+};
+let held = "";
+
+// Start here. What is this function doing?
+const printStacks = () => {
+  console.log("a: " + stacks.a);
+  console.log("b: " + stacks.b);
+  console.log("c: " + stacks.c);
+};
+
+// Next, what do you think this function should do?
+const movePiece = (startStack, endStack) => {
+  held = stacks[startStack].pop();
+  stacks[endStack].push(held);
+};
+
+// Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
+const isLegal = (start, end) => {
+  if (
+    stacks[end].length === 0 ||
+    stacks[start].slice(-1)[0] < stacks[end].slice(-1)[0] ||
+    start === end
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//valid input format and check
+const validInput = (input1, input2) => {
+  if (
+    (input1 === "a" || input1 === "b" || input1 === "c") &&
+    (input2 === "a" || input2 === "b" || input2 === "c")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//logic to check current array vs win-state
+
+function deepEqual(obj1, obj2) {
+  if (obj1 === obj2) {
+    return true;
+  }
+  if (isPrimitive(obj1) && isPrimitive(obj2)) {
+    return obj1 === obj2;
+  }
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
+  for (let key in obj1) {
+    if (!(key in obj2)) return false;
+    if (!deepEqual(obj1[key], obj2[key])) return false;
+  }
+  return true;
+}
+
+function isPrimitive(obj) {
+  return obj !== Object(obj);
+}
+
+function isPrimitive(obj) {
+  return obj !== Object(obj);
+}
+
+// check for win
+
+const checkForWin = () => {
+  if (
+    deepEqual(stacks, { a: [], b: [4, 3, 2, 1], c: [] }) ||
+    deepEqual(stacks, { a: [], b: [], c: [4, 3, 2, 1] })
+  ) {
+    console.log("You Win!");
+    alert("you Win!");
+    winPos = document.getElementById(rowEnd);
+    startPos = document.getElementById("a");
+    while (winPos.firstChild) {
+      let reset = winPos.removeChild(winPos.lastElementChild);
+      startPos.insertBefore(reset, startPos.firstChild);
+    }
+    const stacks = {
+      a: [4, 3, 2, 1],
+      b: [],
+      c: [],
+    };
+
+    rowEnd = null;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// When is this function called? What should it do with its argument?
+const towersOfHanoi = (input1, input2) => {
+  if (isLegal(input1, input2) === true) {
+    movePiece(input1, input2);
+    setTimeout(() => {
+      checkForWin();
+    }, 50);
+  } else {
+    console.error("Invalid placement");
+  }
 };
